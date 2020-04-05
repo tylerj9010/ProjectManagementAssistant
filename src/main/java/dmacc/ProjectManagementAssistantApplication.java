ad336e34f054534e0995c042aba0a807baac5abe
G8A4W0416 <wilhelmg1@gmail.com>
2020-04-05 16:42:48 -0500

    Initial commit

---
+++ src/main/java/dmacc/ProjectManagementAssistantApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementAssistantApplication.class, args);
+	}
+
+}
