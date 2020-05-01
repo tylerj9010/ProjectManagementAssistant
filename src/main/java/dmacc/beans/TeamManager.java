aea42d83c79a338bfe6c533e6796ba9db10c8057
tylerj9010 <54857917+tylerj9010@users.noreply.github.com>
2020-04-30 20:26:40 -0500

    Update TeamManager.java

--- src/main/java/dmacc/beans/TeamManager.java
+++ src/main/java/dmacc/beans/TeamManager.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "teammanager")
+@Table(name = "TeamManager")
 public class TeamManager {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,7 +18,6 @@ public class TeamManager {
 	private String firstName;
 	@Column(name = "lastName")
 	private String lastName;
-	
 	
 	public TeamManager() {
 	}
@@ -33,30 +32,27 @@ public class TeamManager {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
-
-//	public TeamManager(long managerId, String firstName, String lastName, String userName, String password) {
-//		this.managerId = managerId;
-//		this.firstName = firstName;
-//		this.lastName = lastName;
-//		this.userName = userName;
-//		this.password = password;
-//	}
 	
 	public long getManagerId() {
 		return managerId;
 	}
+	
 	public void setManagerId(long managerId) {
 		this.managerId = managerId;
 	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
@@ -65,20 +61,4 @@ public class TeamManager {
 	public String toString() {
 		return "TeamManager [managerId=" + managerId + ", firstName=" + firstName + ", lastName=" + lastName + "]";
 	}
-
-//	public String getUserName() {
-//		return userName;
-//	}
-//	public void setUserName(String userName) {
-//		this.userName = userName;
-//	}
-//	public String getPassword() {
-//		return password;
-//	}
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
-
-
-	
 }
