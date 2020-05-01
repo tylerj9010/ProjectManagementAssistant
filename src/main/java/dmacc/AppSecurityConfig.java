9ea4f7b5a183483dc3d5c2d4097e920640cdc071
tylerj9010 <54857917+tylerj9010@users.noreply.github.com>
2020-04-30 22:01:35 -0500

    pulled changes, resolved confilict with eclipse project

--- src/main/java/dmacc/AppSecurityConfig.java
+++ src/main/java/dmacc/AppSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,11 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 		provider.setPasswordEncoder(new BCryptPasswordEncoder());
 		return provider;
 	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -39,4 +45,16 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 			.logout()                                    
 				.permitAll();
 	}
+
+//TODO figure out how to show features based on role, Team Manager vs. Team Member
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//				.antMatchers("/admin").hasRole("ADMIN")
+//				.antMatchers("/user").hasAnyRole("ADMIN", "USER")
+//				.antMatchers("/").permitAll()
+//				.and().formLogin();
+//	}
+	
+	
 }
